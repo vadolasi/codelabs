5014950246f016bb0f336f2617b5d0ebb251ac6c
Vitor Daniel <vitor036daniel@gmail.com>
2023-07-09 19:40:50 -0300

    add 404 page

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,19 +1,23 @@
-import { Route } from "wouter-preact"
+import { Route, Switch } from "wouter-preact"
 import Editor from "./pages/Editor"
 import { Provider } from "urql"
 import { client } from "./client"
 import { DragDropContext } from "react-beautiful-dnd"
 import { Toaster } from "react-hot-toast"
 import Home from "./pages/Home"
+import Error404 from "./pages/404"
 
 export default function App() {
   return (
     <DragDropContext onDragEnd={() => {}}>
       <Provider value={client}>
-        <Route path="/" component={Home} />
-        <Route path="/room/:id">
-          {({ id }) => <Editor id={id} />}
-        </Route>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/room/:id">
+            {({ id }) => <Editor id={id} />}
+          </Route>
+          <Route component={Error404} />
+        </Switch>
       </Provider>
       <Toaster/>
     </DragDropContext>
---
+++ apps/web/src/pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Error404() {
+  return (
+    <div _w="screen" _h="screen" _flex="~ col" _justify="center" _items="center">
+      <h1>404</h1>
+      <h2>Página não encontrada!</h2>
+    </div>
+  )
+}
