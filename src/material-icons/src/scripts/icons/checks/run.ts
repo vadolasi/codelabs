dc3d128ee21d2558d047532eacf40009ef01b959
Vitor Daniel <vitor036daniel@gmail.com>
2024-09-17 21:41:59 -0300

    🐛 Fix file duplication when creating from file tree

--- src/material-icons/src/scripts/icons/checks/run.ts
+++
@@ -1,7 +0,0 @@
-import { check as checkAvailability } from './checkIconAvailability';
-import { check as checkIconConflicts } from './checkIconConflicts';
-import { check as checkIconUsage } from './checkIconUsage';
-
-checkAvailability();
-checkIconUsage();
-checkIconConflicts();
