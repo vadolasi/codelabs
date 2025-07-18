b56c082f17e3d54875a38266b09e62346ba86cc6
Vitor Daniel <vitor036daniel@gmail.com>
2025-07-17 23:15:10 -0300

    fix: arrumar configuração do svelte

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
+import adapter from "@sveltejs/adapter-node"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-import adapter from "svelte-adapter-bun-next"
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
