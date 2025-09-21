3b44c73ae3a276ec22aa249cd18f9f79802e9310
Vitor Daniel <vitor036daniel@gmail.com>
2025-09-20 22:42:12 -0300

    fix: connect to redis only when is not building

--- web/src/backend/lib/redis.ts
+++ web/src/backend/lib/redis.ts
@@ -1,11 +1,16 @@
+import { building } from "$app/environment"
 import Redis, { RESP_TYPES } from "redis"
 import config from "./config"
 
 const redis = Redis.createClient({
-	url: config.REDIS_URL
+	url: config.REDIS_URL,
+	unstableResp3: true
 }).withTypeMapping({
 	[RESP_TYPES.BLOB_STRING]: Buffer
 })
-await redis.connect()
+
+if (!building) {
+	await redis.connect()
+}
 
 export default redis
