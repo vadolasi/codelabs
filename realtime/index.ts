bb0ba10fcace5819897f22dbe33a7448f948b762
Vitor Daniel <vitor036daniel@gmail.com>
2025-09-20 22:28:34 -0300

    fix: update zod

--- realtime/index.ts
+++ realtime/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!")
+import { Server as Engine } from "@socket.io/bun-engine"
+import { Server } from "socket.io"
+
+const io = new Server()
+
+const engine = new Engine({
+	path: "/socket.io/"
+})
+
+io.bind(engine)
+
+io.on("connection", (socket) => {
+	console.log("a user connected")
+})
+
+export default {
+	port: 3000,
+	...engine.handler()
+}
